94f654068433b039a5ed863d11332ca140b7e153
traveler20 <60683849+traveler20@users.noreply.github.com>
2021-06-03 08:54:42 +0900

    Add files via upload

---
+++ gimmick-site/js-mouse-rotate-effect/js/script.js
@@ -0,0 +1,20 @@
+// -----------------------------
+// mousetext circle effect
+// -----------------------------
+// mousetext definition
+const mousetext = document.querySelector(".js-mousetext");
+
+// mousetext insert span
+mousetext.innerHTML = mousetext.textContent.replace(/\S/g, "<span>$&</span>");
+
+// mousetext span insert rotate element
+const element = document.querySelectorAll("span");
+for (let i = 0; i < element.length; i++) {
+	element[i].style.transform = "rotate(" + i * 18 + "deg)";
+}
+
+// mousetext interlock
+document.addEventListener("mousemove", function (e) {
+	mousetext.style.left = e.pageX + "px";
+	mousetext.style.top = e.pageY + "px";
+});
